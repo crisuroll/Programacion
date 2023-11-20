97528fdea2a58b5936bde471a01155d993a18688
crisuroll <cristina.cg9@hotmail.com>
2023-11-20 19:16:13 +0100

    Explicacion de static

---
+++ Ejemplo04 Static/src/A.java
@@ -0,0 +1,25 @@
+/**
+ * Clase A.
+ * @author crisuroll
+ */
+public class A {
+	
+	private static int x = 5;
+	private int y;
+	
+	public A (int y) {
+		this.y = y;
+	}
+	
+	static public void incrementa() {
+		A.x++;
+		A a = new A(3);
+		a.y++;
+	}
+	
+	public String toString() {
+		return "x = " + A.x +
+				"y = " + this.y;
+	}
+	
+}
---
+++ Ejemplo04 Static/src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Clase Main.
+ * @author crisuroll
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//A a1 = new A(7);
+		
+		A.incrementa();
+
+	}
+
+}
