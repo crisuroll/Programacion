97ba9a3f08ce40ed25ed4837de037f60b15a817d
crisuroll <cristina.cg9@hotmail.com>
2023-11-21 18:58:52 +0100

    Ejercicios finalizados

--- Tipo array/src/arraysDosDimensiones/Ej03.java
+++ Tipo array/src/arraysDosDimensiones/Ej03.java
@@ -14,8 +14,65 @@ public class Ej03 {
 	 */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int[][] matriz = {
+			{5, 10, 15},
+			{20, 25, 30},
+			{35, 40, 45}
+		};
+		
+		System.out.println("Matriz original: ");
+		imprimirMatriz(matriz);
+		
+		calcularPromedioFilas(matriz);
+		calcularPromedioColumnas(matriz);
+		
+	}
+	
+	public static void calcularPromedioFilas(int[][] matriz) {
+		
+		int filas = matriz.length;
+		int columnas = matriz[0].length;
+		
+		System.out.println("Promedio de cada fila:");
+		for (int i = 0; i < filas; i++) {
+			int sumaFila = 0;
+			for (int j = 0; j < columnas; j++) {
+				sumaFila += matriz[i][j];
+			}
+			double promedioFila = (double) sumaFila / columnas;
+			System.out.println("Fila " + (i + 1) + ": " + promedioFila);
+		}
+		
+	}
+	
+	public static void calcularPromedioColumnas(int[][]matriz) {
+		
+		int filas = matriz.length;
+		int columnas = matriz[0].length;
+		
+		System.out.println("Promedio de cada columna:");
+		for (int j = 0; j < columnas; j++) {
+			int sumaColumna = 0;
+			for (int i = 0; i < filas; i++) {
+				sumaColumna += matriz[i][j];
+			}
+			double promedioColumna = (double) sumaColumna / filas;
+			System.out.println("Columna " + (j + 1) + ": " + promedioColumna);
+		}
+		
+	}
+	
+	public static void imprimirMatriz(int[][] matriz) {
+		int filas = matriz.length;
+		int columnas = matriz[0].length;
+		
+		for (int i = 0; i < filas; i++) {
+			for (int j = 0; j < columnas; j++) {
+				System.out.println(matriz[i][j] + "\t");
+			}
+			System.out.println();
+		}
 	}
 
 }
--- Tipo array/src/arraysDosDimensiones/Ej04.java
+++ Tipo array/src/arraysDosDimensiones/Ej04.java
@@ -1,5 +1,7 @@
 package arraysDosDimensiones;
 
+import java.util.Scanner;
+
 /**
  * Clase Ej04. Crea un programa que declare una matriz. Despues, pedira tres veces al usuario 
  * que indique una posicion de la matriz (proporcionando fila y columna), y el programa debe 
@@ -17,8 +19,47 @@ public class Ej04 {
 	 */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		int[][] matriz = {
+			{1, 2, 3},
+			{4, 5, 6},
+			{7, 8, 9}
+		};
+		
+		System.out.println("Matriz original:");
+		imprimirMatriz(matriz);
+		int suma = 0;
+		boolean valida;
+		
+		for (int i = 0; i < 3; i++) {
+			valida = false;
+			while(!valida) {
+				System.out.println("Ingresa la posición (fila y columna) para sumar. Ejemplo: <fila> <columna>");
+				int fila = sc.nextInt();
+				int columna = sc.nextInt();
+				
+				if (fila >= 0 && fila < matriz.length && columna >= 0 && columna < matriz[0].length) {
+					System.out.println("Posición correcta!");
+					suma += matriz[fila][columna];
+					valida = true;
+				} else {
+					System.out.println("Posicion fuera de los limites de la matriz. No se sumará.");
+				}
+			}
+		}
+		System.out.println("La suma de las posiciones seleccionadas es: " + suma);
+		sc.close();
 
+	}
+	
+	public static void imprimirMatriz(int[][] matriz) {
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz [0]. length ; j ++) {
+				System . out . print ( matriz [ i ][ j ] + "\t") ;
+			}
+			System.out.println();
+		}
 	}
 
 }
