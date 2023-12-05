8d3ab402cf57786a38e99c5daeb2414510e2f209
crisuroll <cristina.cg9@hotmail.com>
2023-12-05 17:44:18 +0100

    Algoritmo de insercion, estructura

---
+++ AlgoritmoInsercion/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * for(int i = 1; i < array.length; i++) {
+		 * 		while((i-1 < i) || (i < array.length)) {
+		 * 
+		 * 		}
+		 * }
+		 */
+
+	}
+
+}
